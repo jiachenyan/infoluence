a62db49e81b4fe0de01b7bf2b85ccaa0f76b597a
Jackie Yan - Surface <jiachen.yan@utoronto.ca>
2016-03-12 11:40:53 -0500

    timeline

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,6 +4,7 @@ module PostsHelper
 	def post_projs
 		post_tb = Post.arel_table
 		[
+			post_tb[:id].as('"postId"'),
 			post_tb[:content],
 			arel_sql_epoch(post_tb, :created_at, :timestamp)
 		]
