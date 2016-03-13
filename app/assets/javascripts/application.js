4f5ffd2a7abc01696b1e64885cfad37179215feb
Han Qiu <donkwagon@hotmail.com>
2016-03-13 10:55:03 -0400

    building search

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
 //= require d3
+//= require jquery
 //= require jquery_ujs
 //= require angular
 //= require angular-cookies
